863f8efe98da4d8481d996dc9c94f71a049d767a
Emmanuel0435 <emmanuel.beltran4241@alumnos.udg.mx>
2025-04-10 19:12:14 -0600

    Create Helloworld.sh

---
+++ Hands-on 3/Helloworld.sh
@@ -0,0 +1,16 @@
+#Imprimimos en pantalla
+echo "Hello World"
+
+#Listamos el contenido del directorio que nos encontramos
+echo "Listado del contenido: "
+ls
+
+#Creamos un directorio
+mkdir -p Test
+
+#Nos cambiamos a ese directorio
+cd Test
+
+#Listamos Test
+echo "Contenido de Test: "
+ls
